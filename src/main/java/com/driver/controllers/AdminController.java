d0583cc6df07a0b6259f323bc82ebb521ca32fff
SakT1122 <sak.978tirbhane@gmail.com>
2023-03-04 15:12:42 +0530

    commit

--- src/main/java/com/driver/controllers/AdminController.java
+++ src/main/java/com/driver/controllers/AdminController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.driver.model.Admin;
+import com.driver.model.ServiceProvider;
+
 
 @RestController
 @RequestMapping("/admin")
